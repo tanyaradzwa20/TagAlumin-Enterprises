2d4658d58da096bc3e2be87797ddde8fa95da48b
Tanya <tanya@uncommon.org>
2022-02-02 11:33:12 +0200

    new

---
+++ main.js
@@ -0,0 +1,18 @@
+// load more button
+var numberOfItems=6;
+$('#myList .col-md-4:lt('+numberOfItems+')').fadeIn();
+
+
+$('#loadMore').click(function () {
+    numberOfItems = numberOfItems+6;
+    $('#myList .col-md-4:lt('+numberOfItems+')').fadeIn();
+            $('#loadMore').css("display", "none");
+    $('#showLess').css("display", "block");
+});
+
+$('#showLess').click(function () {
+    numberOfItems= numberOfItems-6;
+    $('#myList .col-md-4').not(':lt('+numberOfItems+')').fadeOut();
+    $('#loadMore').css("display", "block");
+    $('#showLess').css("display", "none");
+});
